bfd6a822591c9735c485f33af51e37636c4b57d2
Tran Luong <tranluong@Trans-MacBook-Air.local>
2021-03-29 07:52:08 -0700

    styled hamburger menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,46 +2,28 @@ import {useState} from 'react'
 import {Link} from "react-router-dom"
 import {List, ListItem, AppBar, IconButton, Toolbar, Drawer} from "@material-ui/core"
 import {Menu, ChevronLeft} from '@material-ui/icons';
-import { makeStyles } from '@material-ui/core/styles'
-
-
-const useStyles = makeStyles({
-    root: {
-        // display: "flex",
-        // flexDirection: "row",
-        // justifyContent: "center",
-        // backgroundColor: "black",
-    },
-    item: {
-    textDecoration: "none",
-    // color: "white",
-
-    },
-  });
+import "./styles/Nav.css"
 
 function Nav() {
-    const classes = useStyles()
     const [toggleMenu, setToggleMenu] = useState(false)
     return (
         <div className="nav">
-            <AppBar>
-                <Toolbar>
-
-                <IconButton onClick={() => setToggleMenu((prevState) => !prevState)}edge="start" aria-label="menu">
-                    <Menu />
-                </IconButton>
+            <AppBar >
+                <Toolbar className="nav-bar">
+                    <IconButton onClick={() => setToggleMenu((prevState) => !prevState)}edge="start" aria-label="menu">
+                        <Menu className="icon"/>
+                    </IconButton>
                 </Toolbar>
-                </AppBar>
+            </AppBar>
                 <Drawer variant="persistent" anchor="left" open={toggleMenu}>
-                <IconButton onClick={() => setToggleMenu((prevState) => !prevState)}edge="start" aria-label="menu">
-                    <ChevronLeft />
-                </IconButton>
-                    
-                    <List className={classes.root}>
-                    <ListItem ><Link className={classes.item} to="/goals">FishGoals</Link></ListItem>
-                    <ListItem><Link className={classes.item} to="/caught">Fish Caught</Link></ListItem>
-                    <ListItem><Link className={classes.item} to="/missed">Fish Missed</Link></ListItem>            
-                    <ListItem><Link className={classes.item} to="/">Log Out</Link></ListItem>
+                    <IconButton onClick={() => setToggleMenu((prevState) => !prevState)}edge="start" aria-label="menu">
+                        <ChevronLeft />
+                    </IconButton>
+                    <List>
+                        <ListItem ><Link className="item" to="/goals">FishGoals</Link></ListItem>
+                        <ListItem><Link className="item" to="/caught">Fish Caught</Link></ListItem>
+                        <ListItem><Link className="item" to="/missed">Fish Missed</Link></ListItem>            
+                        <ListItem><Link className="item" to="/">Log Out</Link></ListItem>
                     </List>
                 </Drawer>
     
