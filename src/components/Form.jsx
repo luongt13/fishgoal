660a64d18f8eb69e753f4fca9bd62d3daf8e0eaa
Tran Luong <tranluong@Trans-MacBook-Air.local>
2021-04-01 08:56:20 -0700

    test user login and signup logic

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -13,41 +13,81 @@ function Form(props) {
     const [name, setName] = useLocalStorage("name",'')
     const [username, setUsername] = useLocalStorage("username","")
 
-    // const [name, setName] = useState("")
-    // const [username, setUsername] = useState("")
-    // const [userName, setUserName] = useLocalStorage("name",'')
-    // const [userId, setUserId] = useLocalStorage("username","")
     let { title } = useParams()
     let history = useHistory()
     const [userData, setUserData] = useState([])
     useEffect(() => {
         handleUser()
     }, [])
-    userData.map(userInfo => {
-        console.log(userInfo.fields)
-    })
-    // let storedUserId = JSON.parse(localStorage.getItem("username"))
-    // let storedUserName = localStorage.getItem("name")
-    // console.log(storedUserId)
-    // function handleForm(){
-    //     if(title === "login") {
-    //         if (storedUserName === name || storedUserId === username) {
-    //             props.setToggle(prevState => !prevState)
-    //             history.push("/goals")
-    //         } else {
-    //             history.push("/form/sign-up")
-    //         }
+console.log(userData)
+    function checkInput(event) {
+        event.preventDefault()
+        userData.map(user => {
+            console.log(Object.values(user))
+            let currentUsers = Object.values(user)
+        
+            // let ah = currentUsers.find(userName => userName === name)
+            // let moo = currentUsers.find(userName => userName === username)
+            
+            console.log(currentUsers.find(userName => userName === name))
+            console.log(currentUsers.find(userName => userName === username))
+            if (title === "login") {
+                    if(currentUsers.find(userName => userName === name) && currentUsers.find(userName => userName === username)) {
+                        console.log("success")
+                        return handleRedirect()
+                        } else {
+                            console.log("Invalid username and password")
+                        }
+                } else if (title === "sign-up"){
+                    if(currentUsers.find(userName => userName === username)) {
+                        console.log("username is taken")
+                    } else if(name.length > 2 && username.length > 2){
+                        // return registerUser()
+                        console.log(username)
+                    }
+                }
+            })
+           
 
-    //     } else {
-    //         setUserName(name)
-    //         setUserId(username)
-    //         history.push("/form/login")
-    //     }
-    // }
+        }
+        // console.log(tryThis)
+        // users.map(user => {
+        //         console.log(Object.values(user))
+        //         let currentUsers = Object.values(user)
+
+
+        //         if (title === "login") {
+        //             if(currentUsers[0] === name && currentUsers[1] === username) {
+        //                 console.log("success")
+        //                 return handleRedirect()
+        //                 } else {
+        //                     console.log("Invalid username and password")
+        //                 }
+        //         } else if (title === "sign-up"){
+        //             if(currentUsers[1] === username) {
+        //                 console.log("username is taken")
+        //             } else if(name.length > 2 && username.length > 2){
+        //                 // return registerUser()
+        //                 console.log(username)
+        //             }
+        //         }
+        //     })
+           
+        // })
+    
+
+    async function registerUser() {
+        let newUser = {
+            name,
+            username,
+        }
+        console.log(newUser)
+        await axios.post(userURL, {fields: newUser}, config)
+    }
 
     async function handleUser() {
         let resp = await axios.get(userURL, config)
-        setUserData(resp.data.records)
+        setUserData(resp.data.records.map(user => user.fields))
     }
     //login or sign-up heading
     function showForm() {
@@ -75,7 +115,7 @@ function Form(props) {
     return (
         <div>
             {showForm()}
-            <form onSubmit={handleRedirect}>
+            <form onSubmit={checkInput}>
                 <Card className="form-container">
                     <CardContent className="form-item">
                         <TextField type="text" id="name" value={name} variant="outlined" label="Name" onChange={(e) => setName(e.target.value)} required/>
